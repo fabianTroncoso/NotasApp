b4d8b3f20bd1386aebb7bb8da978e385cee22d9c
TRISTAN MACIAS <e.tristan.m@hotmail.com>
2021-08-10 23:15:13 -0500

    Arreglando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 //import { CommonModule } from '@angular/common';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   //{ path: '', component: AppComponent },
-  { path: 'users', component: UsersComponent }
+  { path: 'users', component: UsersComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent }
 ]
 
 
