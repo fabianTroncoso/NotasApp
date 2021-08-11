2f733a83f89866ee452265195b24f8491fd36aae
fabianTroncoso <troncosoreyes.fabian@gmail.com>
2021-08-10 23:20:11 -0500

    agregando mi compononte crear notas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CrearNotasComponent } from './crear-notas/crear-notas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearNotasComponent
   ],
   imports: [
     BrowserModule
