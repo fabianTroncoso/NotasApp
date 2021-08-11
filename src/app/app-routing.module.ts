43ddf048935da258af4838f78e58503baab69f4f
fabianTroncoso <troncosoreyes.fabian@gmail.com>
2021-08-11 12:23:59 -0500

    AGREGUE FORMULARIO  A COMPONENETE CREARNOTAS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { CrearNotasComponent } from './crear-notas/crear-notas.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  { path: "register", component: RegisterComponent },
+  { path: "newNotes", component: CrearNotasComponent }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/crear-notas/crear-notas.component.ts
+++ src/app/crear-notas/crear-notas.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'; 
+import { Nota } from './notas.module';
+import { FormsModule,FormGroup, FormControl }   from '@angular/forms';
 
 @Component({
   selector: 'app-crear-notas',
   templateUrl: './crear-notas.component.html',
-  styleUrls: ['./crear-notas.component.css']
+  styleUrls: ['./crear-notas.component.css'],
+  exportAs: 'ngForm'
 })
 export class CrearNotasComponent implements OnInit {
 
   constructor() { }
+  notas: Nota[]=[]
+  nota: Nota={
+    titulo: "",
+    descripcion: "",
+    fechaAper: new Date(),
+    tipo: "",
+    estado:""
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(fn: NgForm): void { 
+
+    const { titulo, descripcion, fechaAper, tipo, estado} = fn.value
+    this.nota={
+      titulo: titulo,
+      descripcion: descripcion,
+      fechaAper: fechaAper,
+      tipo: tipo,
+      estado: estado
+    }
+    this.notas.push(this.nota)
+  }
 }
